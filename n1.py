1e7fb31b5845c347b65f3506bc2ef5396ecaa14a
Servelatus <megeve@yandex.ru>
2021-10-07 23:20:26 +0300

    Домашка №1

---
+++ n1.py
@@ -0,0 +1,12 @@
+# 1. Поработайте с переменными, создайте несколько,
+# выведите на экран, запросите у пользователя несколько чисел и строк
+# и сохраните в переменные, выведите на экран.
+
+first_var = 1
+second_var = 2.0
+third_var = 'three'
+print(first_var, second_var, third_var)
+first_var = input('Введите что-нибудь ')
+second_var = input('Введите строчку ')
+third_var = int(input('Введите число '))
+print(first_var, second_var, third_var)
